a5abd1d1c5e25ffb391f57eb6802341d385e9616
Junjie Su <jsu@c2r5s3.42singapore.sg>
2024-08-01 17:01:02 +0800

    initial test cases passed for builtin_cd

--- hubert/builtin_cd_pwd.c
+++ hubert/builtin_cd_pwd.c
@@ -20,14 +20,18 @@ The buf char array is also set the pathname
 
 NOTE. the calling function will handle any redirect from STDOUT to alternate FD?
 */
-void	builtin_pwd(char **arg, char ***envpc)
+int	builtin_pwd(char **arg, char ***envpc)
 {
 	(void)arg;
 	char	buf[PATH_MAX];
 	
 	if (getcwd(buf, PATH_MAX) == NULL)
-		return ;
-	printf("%s\n", buf); // else if getcwd() success, print pathname to STDOUT
+		return (1);
+	else
+	{
+		printf("%s\n", buf); // else if getcwd() success, print pathname to STDOUT
+		return (0);
+	}
 }
 
 /*
@@ -74,7 +78,7 @@ static int len_ch_star_star(char **css)
 	return (index);
 }
 
-void	builtin_cd(char **arg, char ***envpc)
+int	builtin_cd(char **arg, char ***envpc)
 {
 	char	*dst_dir;
 	char	*tmp;
@@ -82,52 +86,87 @@ void	builtin_cd(char **arg, char ***envpc)
 	if (len_ch_star_star(arg) >= 2)
 	{
 		ft_putstr_fd("minishell: cd: too many arguments", 2);
-		return ;
+		return (1);
 	}
-	if (len_ch_star_star(arg) == 0)
+
+	else if (len_ch_star_star(arg) == 0)
 	{
 		dst_dir = envpc_get_value(*envpc, "HOME");
 		if (dst_dir == NULL)
 		{
 			ft_putstr_fd("minishell: cd: HOME not set", 2);		
-			return ;
+			return (1);
 		}
-	}
-	if (len_ch_star_star(arg) == 1)
-	{
-		dst_dir = arg[0];
-		if (ft_strncmp(dst_dir, "", 1) == 0)
-			return ;
-		if (chdir(dst_dir) == 0)
+		else
 		{
-			tmp = envpc_get_value(*envpc, "PWD"); // replace w/ getcwd()
-			envpc_add(envpc, "OLDPWD", tmp);
-			envpc_add(envpc, "PWD", dst_dir);
-		}
-		else if (chdir(dst_dir) == -1)
-		{
-			if (access(dst_dir, F_OK) == -1)
+			if (chdir(dst_dir) == 0)
+			{
+				tmp = envpc_get_value(*envpc, "PWD");
+				envpc_add(envpc, "OLDPWD", tmp);
+				envpc_add(envpc, "PWD", dst_dir);		
+				return (0);
+			}
+			else if (chdir(dst_dir) == -1)
 			{
 				ft_putstr_fd("minishell: cd: ", 2);
 				ft_putstr_fd(dst_dir, 2);
-				ft_putstr_fd(" no such file or directory", 2);
+				if (access(dst_dir, F_OK) == -1)
+				{
+					ft_putstr_fd(" no such file or directory", 2);			
+					return (1);
+				}
+				else if (access(dst_dir, R_OK) == -1)
+				{
+					ft_putstr_fd(" permission denied", 2);
+					return (1);
+				}
+				else
+				{
+					ft_putstr_fd(" not a directory", 2);
+					return (1);
+				}
+			}
+		}
+	}
+
+	else if (len_ch_star_star(arg) == 1)
+	{
+		dst_dir = arg[0];
+		if (ft_strncmp(dst_dir, "", 1) == 0)
+			return (0);
+		else if (chdir(dst_dir) == 0)
+		{
+			tmp = envpc_get_value(*envpc, "PWD");
+			envpc_add(envpc, "OLDPWD", tmp);
+			envpc_add(envpc, "PWD", dst_dir);
+			return (0);
+		}
+		else if (chdir(dst_dir) == -1)
+		{
+			ft_putstr_fd("minishell: cd: ", 2);
+			ft_putstr_fd(dst_dir, 2);
+			if (access(dst_dir, F_OK) == -1)
+			{
+				ft_putstr_fd(" no such file or directory", 2);			
+				return (1);
 			}
 		    else if (access(dst_dir, R_OK) == -1)
 		    {
-				ft_putstr_fd("minishell: cd: ", 2);
-				ft_putstr_fd(dst_dir, 2);
 				ft_putstr_fd(" permission denied", 2);
-			}
+		    	return (1);
+		    }
 		    else
 		    {
-				ft_putstr_fd("minishell: cd: ", 2);
-				ft_putstr_fd(dst_dir, 2);
 				ft_putstr_fd(" not a directory", 2);
-			}
-		}	
+				return (1);
+		    }
+		}
 	}
+
+	return (0);
 }
 
+/*
 int	main(int ac, char *av[], char **env)
 {
 	char	**envpc;
@@ -148,81 +187,17 @@ int	main(int ac, char *av[], char **env)
 	
 	printf("bef:\n");
 	builtin_pwd(NULL, &env);
-	
-	arg[0] = "./learn";
-	arg[1] = NULL;
-	
-	
-	test_rel_minus_1 = "./learn"; // relative path, directory nested in current directory
-	test_rel_plus_1 = ".."; // relative path, one level up
-	test_rel_plus_2 = "../.."; // relative path, TWO levels up
-	test_abs = "/usr/games"; // absolute path
-	test_fail_1 = "/etc/passwd"; // expect fail, because arg is file, not dir
-	test_fail_2 = ""; // empty pathname
-	test_fail_3 = "123 hello world"; // some random str
-	root = "/";
-	oldpwd = envpc_get_value(env, "OLDPWD");
-	no_arg = NULL;
+	printf("\n");
 
-// relative path, directory nested in current directory, ie. ONE level below current directory 
-	printf("\nchdir() w/ rel path, ONE level BELOW current directory:\n");
-	builtin_cd(&test_rel_minus_1, &envpc);
-	builtin_pwd(NULL, &envpc);
-
-// relative path, ONE level up
-	printf("\nchdir() w/ rel path, ONE level up:\n");
-	builtin_cd(&test_rel_plus_1, &envpc);
-	builtin_pwd(NULL, &envpc);
-	
-// relative path, TWO levels up
-	printf("\nchdir() w/ rel path, TWO levels up:\n");
-	builtin_cd(&test_rel_plus_2, &envpc);
-	builtin_pwd(NULL, &envpc);
-
-// abs path	
-	printf("\nchdir() w/ abs path:\n");
-	builtin_cd(&test_abs, &envpc);
-	builtin_pwd(NULL, &envpc);
-
-// file as arg
-	printf("\nchdir() w/ an existing file as arg:\n");
-	builtin_cd(&test_fail_1, &envpc);
-	//builtin_pwd(NULL, &envpc);
-	
-// some random str as arg
-	printf("\nchdir() w/ some random str as arg:\n");
-	builtin_cd(&test_fail_3, &envpc);
-	//builtin_pwd(NULL, &envpc);
-	
-// abs path, root
-	printf("\nchdir() to root:\n");
-	builtin_cd(&root, &envpc);
-	builtin_pwd(NULL, &envpc);
-	
-// abs path, $OLDPWD
-	printf("\nchdir() to OLDPWD, from env:\n");
-	builtin_cd(&oldpwd, &envpc);
-	builtin_pwd(NULL, &envpc);
-
-//  SPECIAL CASES:
-// 	empty str as arg
-	printf("\nchdir() w/ empty str as arg:\n");
-	builtin_cd(&test_fail_2, &envpc);
-	builtin_pwd(NULL, &envpc);
-
-// no arg
-	printf("\nchdir() with no arg, from env:\n");
-	builtin_cd(&no_arg, &envpc);
-	builtin_pwd(NULL, &envpc);
-	
 	char	*multi_arg[3];
 	
-	multi_arg[0] = "hello";
-	multi_arg[1] = "world";
+	//multi_arg[0] = "hello";
+	multi_arg[0] = "./cannotread";
+	//multi_arg[1] = "world";
+	multi_arg[1] = NULL;
 	multi_arg[2] = NULL;
 	
-// more than one arg
-	printf("\nchdir() with more than one arg\n");
+	//printf("\nchdir() with more than one arg\n");
 	builtin_cd(multi_arg, &envpc);
 	printf("\n");
 	builtin_pwd(NULL, &envpc);	
@@ -232,4 +207,13 @@ int	main(int ac, char *av[], char **env)
 	
 	return (0);
 }
-// cc builtin_cd_pwd.c builtin_env.c check_file_status.c ../Libft/libft.a
+cc builtin_cd_pwd.c builtin_env.c printerror.c ../Libft/libft.a
+
+TEST CASES:
+
+absolute path, valid AND invalid
+relative path, valid AND invalid
+"" // empty str
+NULL char ** array
+valid dir, with no read permissions
+*/
