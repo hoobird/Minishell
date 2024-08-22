d410e3b701128a722a6bbfc985575f2dea480500
hoobird <hulim@student.42singapore.sg>
2024-08-22 21:22:25 +0800

    addded envp initialiser and takes into account for starting without enviroment

--- src/main.c
+++ src/main.c
@@ -54,6 +54,27 @@ int	main_exit_free_envpc(char ***envpc)
 	return (i);
 }
 
+void	startup(char **envp, char ***envpc)
+{
+	char	*value;
+	char	*tmp;
+	char	buf[PATH_MAX];
+
+	*envpc = envp_copy(envp);
+	envpc_add(envpc, "?", "0");
+	getcwd(buf, PATH_MAX);
+	envpc_add(envpc, "PWD", buf);
+	value = envpc_get_value(*envpc, "SHLVL");
+	if (value == NULL)
+		envpc_add(envpc, "SHLVL", "1");
+	else
+	{
+		tmp = ft_itoa(ft_atoi(value) + 1);
+		envpc_add(envpc, "SHLVL", tmp);
+		free(tmp);
+	}
+}
+
 // cc main.c builtin_cd_pwd.c  builtin_echo.c builtin_exit.c  execute.c redirection.c piping.c parsing.c token_linkedlist.c printerror.c builtin_env.c expand_shell_var.c check_file_status.c -lreadline ../Libft/libft.a -g
 // valgrind --leak-check=full --show-leak-kinds=all --suppressions=../readline.supp ./a.out
 // valgrind --leak-check=full --show-leak-kinds=all --suppressions=../readline.supp --track-fds=yes --trace-children=yes ./a.out 
@@ -67,8 +88,7 @@ int main(int argc, char *argv[], char *envp[])
 	t_token					**tokenlistlist;
 	t_command_args			**command_args_list;
 
-	envpc = envp_copy(envp); // later then move to the top cuz i need to check if memory leak is not from not freeing envpc
-	envpc_add(&envpc, "?", "0");
+	startup(envp, &envpc);
 	while (1)
 	{
 		// g_received_signal = 0;
